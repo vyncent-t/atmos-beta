6664caebf7380eca5698b0aa985bc03176cde0e9
Vincent T <vyncode@gmail.com>
2022-04-28 17:09:21 -0400

    working on improving auto play

--- client/src/components/musicControls.js
+++ client/src/components/musicControls.js
@@ -5,11 +5,15 @@ import ResumeButton from "./resumeButton"
 import { useEffect, useState } from "react"
 
 
+const axios = require('axios')
+
 function MusicControls(props) {
 
+    let token = localStorage.getItem("spotifyToken")
 
     // next and previous logic for songs
     const [songArrayNum, setSongArrayNumber] = useState(0)
+    const [autoPlayOn, setAutoPlayOn] = useState(true)
 
     function nextSongHandler() {
         setSongArrayNumber(songArrayNum + 1)
@@ -18,8 +22,6 @@ function MusicControls(props) {
     function prevSongHandler() {
         setSongArrayNumber(songArrayNum - 1)
     }
-
-    // let currentSong = musicArrayList[songArrayNum]
 
     const [currentSongInfo, setCurrentSongInfo] = useState(null)
 
@@ -34,18 +36,8 @@ function MusicControls(props) {
     let musicArrayList = props.songList
 
 
-    // let currentSong = props.songList[songArrayNum]
-
     console.log("controls array")
     console.log(musicArrayList)
-
-    // let numSongsAvailable = musicArrayList.length
-
-    // console.log(`current music control list is ${numSongsAvailable} songs long`)
-
-
-
-    // const [nowSong, setNowSong] = useState("now song")
 
 
 
@@ -67,15 +59,50 @@ function MusicControls(props) {
 
 
             songMaker(props.songList[songArrayNum].track)
-
-            // if (!currentSong.track.name) return
-
-
         }, [songArrayNum]
+    )
+
+    useEffect(
+        () => {
+            if (!currentSong) return
+
+
+            playSong(currentSong.uri)
+        }, [currentSong]
     )
 
 
     console.log("data from current song in song conrols")
+
+
+    function playSong(songCode) {
+        axios.post('/spotify-play-music', {
+            userData: {
+                accessToken: `${token}`,
+                musicURI: songCode
+            }
+        }).then(
+            (res) => {
+                console.log("play worked")
+                console.log(res)
+            }
+        ).catch(
+            (err) => {
+                console.log(err)
+            }
+        )
+    }
+
+    function autoPlayOnHandler(song) {
+        setAutoPlayOn(true)
+        playSong(song)
+    }
+
+    function autoPlayOffHandler(song) {
+        setAutoPlayOn(false)
+        // playSong(song)
+    }
+
 
 
     return (
@@ -87,21 +114,34 @@ function MusicControls(props) {
                             music controls
                             <div>
                                 <div>
+                                    {
+                                        autoPlayOn ? (
+                                            <div>
+                                                <button className="btn btn-success" onClick={
+                                                    () => { autoPlayOffHandler() }
+                                                } >Disable Auto Play</button>
+                                            </div>
+                                        ) : <div>
+                                            <button className="btn btn-success" onClick={
+                                                () => { autoPlayOnHandler(currentSong.uri) }
+                                            }>Enable Auto Play</button>
+                                        </div>
+                                    }
                                     <div>
                                         now listening to {currentSong.name} {currentSong.uri}
                                     </div>
                                 </div>
                                 <div>
                                     {songArrayNum > 0 &&
-                                        <button className="btn btn-light" onClick={prevSongHandler}>prev track</button>
+                                        <button className="btn btn-light m-1" onClick={prevSongHandler}>prev track</button>
                                     }
 
                                     {
                                         songArrayNum < 100 &&
-                                        <button className="btn btn-light" onClick={nextSongHandler}>next track</button>
+                                        <button className="btn btn-light m-1" onClick={nextSongHandler}>next track</button>
                                     }
                                 </div>
-                                <PlayButton song={currentSong.uri} />
+                                {/* <PlayButton song={currentSong.uri} /> */}
                                 <PauseButton />
                                 <ResumeButton />
                             </div>
