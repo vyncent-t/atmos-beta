628545e76581d347cec2ce4a982e216f576ae6f5
Vincent T <vyncode@gmail.com>
2022-05-13 13:24:56 -0400

    move src folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from "../src/store/index"
+import store from "./store/index"
 import './index.css'
 import App from './App';
 
